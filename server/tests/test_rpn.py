b59dc9f2abf69c2841c71bdb576d20d701b25b78
dasporal <dasporal@gmail.com>
2024-10-12 17:59:43 +0200

    feat: rpn tests

---
+++ server/tests/test_rpn.py
@@ -0,0 +1,22 @@
+from main import rpn
+import pytest
+
+def test_rpn_calculator_valid():
+    assert rpn(["3", "4", "+", "2", "*"]) == 14
+    assert rpn(["5", "1", "2", "+", "4", "*", "+", "3", "-"]) == 14
+
+def test_rpn_calculator_invalid_too_many_operators():
+    with pytest.raises(ValueError, match="not enough operands for the operation"):
+        rpn(["3", "4", "2", "*", "+", "*"])
+
+def test_rpn_calculator_division_by_zero():
+    with pytest.raises(ValueError, match="Division by zero"):
+        rpn(["10", "0", "/"])
+
+def test_rpn_calculator_unrecognized_operator():
+    with pytest.raises(ValueError, match="Unsupported operator"):
+        rpn(["3", "4", "%"])
+
+def test_rpn_calculator_invalid_final_stack():
+    with pytest.raises(ValueError, match="Invalid expression: too many or too few operators."):
+        rpn(["3", "4", "2", "*"])
