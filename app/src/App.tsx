2ff55f3df6a7878987062757843f0ae7c222dd1b
dasporal <dasporal@gmail.com>
2024-10-12 19:47:28 +0200

    feat(app): init react app with Tailwind and Shadcn's UI

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="flex h-screen w-screen flex-col items-center justify-center">
+			<h1 className="text-2xl">Hello Ayomi!!!</h1>
+		</div>
+	);
+}
+
+export default App;
